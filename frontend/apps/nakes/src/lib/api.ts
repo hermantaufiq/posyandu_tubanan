2a6d68c33a118a0e674d003b9deea37520587474
Herman <hermantaufiq12@gmail.com>
2026-07-26 01:09:24 +0700

    fix: update api url to localtunnel and add bypass header

--- frontend/apps/nakes/src/lib/api.ts
+++ frontend/apps/nakes/src/lib/api.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api',
-  headers: {
+  baseURL: 'https://sipo-backend.loca.lt/api',
+  headers: { "Bypass-Tunnel-Reminder": "true",
     'Content-Type': 'application/json',
     'Accept': 'application/json',
   },
