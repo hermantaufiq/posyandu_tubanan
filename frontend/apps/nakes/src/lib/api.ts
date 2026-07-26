d22462ebee13fb5847ad6aec6dc7924eaa3826f1
Herman <hermantaufiq12@gmail.com>
2026-07-26 22:12:01 +0700

    fix: update localtunnel subdomain to sipo-api-tubanan

--- frontend/apps/nakes/src/lib/api.ts
+++ frontend/apps/nakes/src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://sipo-backend.loca.lt/api',
+  baseURL: 'https://sipo-api-tubanan.loca.lt/api',
   headers: { "Bypass-Tunnel-Reminder": "true",
     'Content-Type': 'application/json',
     'Accept': 'application/json',
